1760936e0bd493b42843835342ddcb7f11af6b82
Hugo Rodrigues <me@hugorodrigues.com>
2018-02-17 16:04:51 -0700

    feat: File encryption module

---
+++ src/lib/File.js
@@ -0,0 +1,38 @@
+const Process = require("./Process")
+
+/**
+ * Encrypts and decrypts files
+ * 
+ * @class File
+ */
+class File {
+
+  /**
+   * Encrypt a single file using the provided key
+   * @param {String} origin Path location
+   * @param {String} destination Path location
+   * @param {String} key
+   */
+  fileEncrypt(origin, destination, key) {
+    // Encrypt origin to destination passing the secret via env (use FS as less as possible)
+    return Process.openssl(`aes-256-cbc -in ${origin} -out ${destination} -pass env:secretKey`, null, {
+      env: { secretKey: key }
+    })
+  }
+
+  /**
+   * Decrypt a single file using the provided key
+   * @param {String} origin Path location
+   * @param {String} destination Path location
+   * @param {String} key
+   */
+  fileDecrypt(origin, destination, key) {
+    // Decrypt origin to destination passing the secret via env (use FS as less as possible)
+    return Process.openssl(`aes-256-cbc -d -in ${origin} -out ${destination} -pass env:secretKey`, null, {
+      env: { secretKey: key }
+    })
+  }
+
+}
+
+module.exports = new File()
---
+++ src/lib/__test__/File.test.js
@@ -0,0 +1,45 @@
+const fs = require("fs-extra")
+const tmp = require("tmp")
+
+const appRoot = require("app-root-path")
+const fixturesPath = `${appRoot}/test/fixtures`
+
+const File = require("../File")
+const Crypto = require("../Crypto")
+
+describe("File.js", () => {
+  test("Encrypt and decrypt file", async () => {
+    // Create temp folder
+    const tempFolder = tmp.dirSync({ unsafeCleanup: true })
+    // console.log("tempFolder", tempFolder.name)
+
+    // Generate new random key for this encryption
+    const encryptionKey = await Crypto.randomSymmetricKey()
+
+    // Sample data to be encrypted
+    const exampleSecret = "Hello world!"
+
+    // Create a temporary file with data
+    await fs.outputFile(`${tempFolder.name}/example.txt`, exampleSecret)
+
+    // Encrypt
+    await File.fileEncrypt(`${tempFolder.name}/example.txt`, `${tempFolder.name}/example.txt.enc`, encryptionKey)
+
+    // Read encrypted version
+    const encryptedVersion = await fs.readFile(`${tempFolder.name}/example.txt.enc`, "utf8")
+
+    // Test if its really encrypted
+    expect(encryptedVersion).not.toBe(exampleSecret)
+
+    // Decrypt
+    await File.fileDecrypt(`${tempFolder.name}/example.txt.enc`, `${tempFolder.name}/example.txt.dec`, encryptionKey)
+
+    // Read decrypted version
+    const decryptedVersion = await fs.readFile(`${tempFolder.name}/example.txt.dec`, "utf8")
+
+    // Test if its really encrypted
+    expect(decryptedVersion).toBe(exampleSecret)
+
+    tempFolder.removeCallback()
+  })
+})
